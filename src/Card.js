a8a79d5bb0250e4bdfeeb5d22ed7677104aa4866
Dave Nadeau <37254473+DaveNadeau@users.noreply.github.com>
2019-10-19 15:54:39 -0700

    stage 1 build complete

---
+++ src/Card.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const Card = ({name, email, id}) => { //destructuring in arguments no instead of "props"
+                                      //see below comment for another option (slightly less good)
+
+  // const {name, email, id} = props; //destructuring to avoid props.id,
+  //                                  //props.email below, just typing id, email, etc.
+  
+  return(
+    <div className= "tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img src={`https://robohash.org/${id}?200x200`} alt="no data present"/>
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
