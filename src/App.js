e0b8024d1f0351231265d89f64ab7bd40986ee90
Dave Nadeau <37254473+DaveNadeau@users.noreply.github.com>
2019-10-21 22:06:18 -0700

    stage 3 build completed

--- src/App.js
+++ src/App.js
@@ -5,19 +5,30 @@ import SearchBox from './SearchBox';
 
 class App extends Component {
   constructor() {
-    super(); //used to avoid error using "this" try without to see error
+    super(); //used to avoid error using "this" - try without to see error
     this.state = {
-      robots: robots,
+      //App component has 2 states: robots and searchfield
+      robots: robots,  //'robots' is now a state in the App constructor, see "render" function, CardList, though
       searchfield: ''
     }
   }
 
+//OnSearchChange is passed to SearchBox below as a prop
+  OnSearchChange = (event) => {
+    //console.log(event.target.value);  //event.target.value updates with actual input in console
+    this.setState({ searchfield: event.target.value }); //setState is a react library function used to update states
+  }
+
   render(){
+    const filteredRobots = this.state.robots.filter(robot =>{
+      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    })
+    //Note that robots (a state of App) is passed as a prop here to CardList
     return (
       <div className = 'tc'>
         <h1>Robofriends</h1>
-        <SearchBox />
-        <CardList robots = {this.state.robots} />
+        <SearchBox searchChange={this.OnSearchChange}/>
+        <CardList robots = {filteredRobots} />
       </div>
     )
   }
